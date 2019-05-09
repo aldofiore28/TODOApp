1b1ed0df3f04eadcb8128acca73254171a1eb4cc
aldofiore28 <aldo.fiore95@gmail.com>
2019-05-09 16:21:44 +0100

    typehinted HomePageController.php

--- src/Classes/Controllers/HomePageController.php
+++ src/Classes/Controllers/HomePageController.php
@@ -2,18 +2,23 @@
 
 namespace Todo\Controllers;
 
+use Slim\Http\Request;
+use Slim\Http\Response;
+use Slim\Views\PhpRenderer;
+use Todo\Models\TodoModel;
+
 class HomePageController
 {
     protected $renderer;
     protected $todoModel;
 
-    public function __construct($renderer, $todoModel)
+    public function __construct(PhpRenderer $renderer, TodoModel $todoModel)
     {
         $this->renderer = $renderer;
         $this->todoModel = $todoModel;
     }
 
-    public function __invoke($request, $response, $args)
+    public function __invoke(Request $request, Response $response, array $args)
     {
         $args['todos'] = $this->todoModel->hydrateTodos();
         $args['completedTodos'] = $this->todoModel->hydrateCompletedTodos();
