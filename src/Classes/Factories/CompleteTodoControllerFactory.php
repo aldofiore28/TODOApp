8c0ec0c0cec62afd4faf6908a6f4329ffc061835
aldofiore28 <aldo.fiore95@gmail.com>
2019-05-09 16:26:36 +0100

    typehinted CompleteTodoControllerFactory.php

--- src/Classes/Factories/CompleteTodoControllerFactory.php
+++ src/Classes/Factories/CompleteTodoControllerFactory.php
@@ -2,11 +2,12 @@
 
 namespace Todo\Factories;
 
+use Psr\Container\ContainerInterface;
 use Todo\Controllers\CompleteTodoController;
 
 class CompleteTodoControllerFactory
 {
-    public function __invoke($container)
+    public function __invoke(ContainerInterface $container) :CompleteTodoController
     {
         $todoModel = $container->get('todoModel');
         return new CompleteTodoController($todoModel);
