96d2681f9650e786ebe4f9c9bd52b6b27930a62f
aldofiore28 <aldo.fiore95@gmail.com>
2019-05-10 11:19:37 +0100

    change variables names to clarify what you get out of it

--- src/Classes/Models/TodoModel.php
+++ src/Classes/Models/TodoModel.php
@@ -23,12 +23,12 @@ class TodoModel
         return TodoHydrator::getCompletedTodos($this->dbConn);
     }
 
-    public function addTodo(string $description, string $date) :bool
+    public function addTodo(string $description, string $deadline) :bool
     {
-        $statement = 'INSERT INTO `todo_list`(`description`, `deadline`) VALUES (:description, :date);';
+        $statement = 'INSERT INTO `todo_list`(`description`, `deadline`) VALUES (:description, :deadline);';
         $query = $this->dbConn->prepare($statement);
         $query->bindParam(':description', $description);
-        $query->bindParam(':date', $date);
+        $query->bindParam(':deadline', $deadline);
         try {
             return $query->execute();
         } catch (\PDOException $exception) {
